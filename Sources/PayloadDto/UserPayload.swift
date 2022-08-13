d85a3dac5282b9dff2b0390597b708d1f34a0f02
Viсtor Chernykh <v-cher9@yandex.ru>
2022-08-13 19:52:16 +0300

    Change UserPayload model

--- Sources/PayloadDto/UserPayload.swift
+++ Sources/PayloadDto/UserPayload.swift
@@ -10,47 +10,41 @@ import Foundation
 /// Payload data for micro services
 public struct UserPayload: Codable {
     // MARK: - Stored properties
-    public let iat: Int     // init at
-    public let userId: UUID
+    public let exp: Double
+    public let jti: UUID
+    public let iat: Double // init at
+    public let sub: UUID
     public let companyId: UUID
     public let domain: String
-    public let email: String
     public let isAdmin: Bool
     public let lang: String
-    public let firstName: String
-    public let lastName: String?
     public let role: RoleType
-    public let space: String?
     public let section: String
     public let tariff: Tariff
     
     // MARK: - Init
     public init(
-        iat: Int,
-        userId: UUID,
+        exp: Double,
+        jti: UUID,
+        iat: Double,
+        sub: UUID,
         companyId: UUID,
         domain: String,
-        email: String,
         isAdmin: Bool,
         lang: String,
-        firstName: String,
-        lastName: String?,
         role: RoleType,
-        space: String?,
         section: String,
         tariff: Tariff
     ) {
+        self.exp = exp
+        self.jti = jti
         self.iat = iat
-        self.userId = userId
+        self.sub = sub
         self.companyId = companyId
         self.domain = domain
-        self.email = email
         self.isAdmin = isAdmin
         self.lang = lang
-        self.firstName = firstName
-        self.lastName = lastName
         self.role = role
-        self.space = space
         self.section = section
         self.tariff = tariff
     }
