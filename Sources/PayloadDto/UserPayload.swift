f3ee004ce8e20eb3faddd9fc82b9cdbfad91cbcd
Viсtor Chernykh <v-cher9@yandex.ru>
2022-08-13 20:34:51 +0300

    Small refactor UserPayload

--- Sources/PayloadDto/UserPayload.swift
+++ Sources/PayloadDto/UserPayload.swift
@@ -13,13 +13,13 @@ public struct UserPayload: Codable {
     public let exp: Double
     public let jti: UUID
     public let iat: Double // init at
-    public let sub: UUID
     public let companyId: UUID
     public let domain: String
     public let isAdmin: Bool
     public let lang: String
     public let role: RoleType
     public let section: String
+    public let sub: UUID
     public let tariff: Tariff
     
     // MARK: - Init
@@ -27,25 +27,25 @@ public struct UserPayload: Codable {
         exp: Double,
         jti: UUID,
         iat: Double,
-        sub: UUID,
         companyId: UUID,
         domain: String,
         isAdmin: Bool,
         lang: String,
         role: RoleType,
         section: String,
+        sub: UUID,
         tariff: Tariff
     ) {
         self.exp = exp
         self.jti = jti
         self.iat = iat
-        self.sub = sub
         self.companyId = companyId
         self.domain = domain
         self.isAdmin = isAdmin
         self.lang = lang
         self.role = role
         self.section = section
+        self.sub = sub
         self.tariff = tariff
     }
 }
